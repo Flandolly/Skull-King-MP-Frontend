38b4bd0e9cc373ce1a32e9cad9c9b25ae7790e6b
Christina Whethers <flandresamarawr@gmail.com>
2021-10-15 11:13:27 -0400

    Started building out landing page to setup for user auth implementations.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
 import React, { useState, useEffect } from "react";
+import {Route, Router} from "react-router-dom";
 import socketIOClient from "socket.io-client";
+import Home from "./Components/Home";
+
+
 const ENDPOINT = "http://127.0.0.1:8080";
 const socket = socketIOClient(ENDPOINT);
 
 function App() {
 
-  const [cookie, setCookie] = useState(null)
-
   useEffect(() => {
     socket.on("connect", () => {
       console.log(socket.id)
     })
-    socket.on("buttonClicked", (data) => {
-      moveButton(data.offsetLeft, data.offsetTop)
-    })
+    // socket.on("buttonClicked", (data) => {
+    //   moveButton(data.offsetLeft, data.offsetTop)
+    // })
+    //
+    // socket.on("buttonPosition", (data) => {
+    //   if(data) {
+    //     moveButton(data.offsetLeft, data.offsetTop)
+    //   }
+    // })
+  }, []);
 
-    socket.on("buttonPosition", (data) => {
-      if(data) {
-        moveButton(data.offsetLeft, data.offsetTop)
-      }
-    })
-  }, [cookie]);
-
-  function moveButton(offLeft, offTop) {
-    const btn = document.getElementById("btn")
-    let top = offTop
-    let left = offLeft
-
-    btn.style.top = top + "px"
-    btn.style.left = left + "px"
-    btn.style.animation = "none"
-  }
-
-  function buttonClicked(event) {
-    console.log("Button clicked")
-    event.currentTarget.style.position = "absolute"
-    socket.emit("buttonClicked", {
-      offsetLeft: Math.random() * ((window.innerWidth - event.currentTarget.clientWidth) - 100),
-      offsetTop: Math.random() * ((window.innerHeight - event.currentTarget.clientHeight) - 50)
-    })
-  }
+  // function moveButton(offLeft, offTop) {
+  //   const btn = document.getElementById("btn")
+  //   let top = offTop
+  //   let left = offLeft
+  //
+  //   btn.style.top = top + "px"
+  //   btn.style.left = left + "px"
+  //   btn.style.animation = "none"
+  // }
+  //
+  // function buttonClicked(event) {
+  //   console.log("Button clicked")
+  //   event.currentTarget.style.position = "absolute"
+  //   socket.emit("buttonClicked", {
+  //     offsetLeft: Math.random() * ((window.innerWidth - event.currentTarget.clientWidth) - 100),
+  //     offsetTop: Math.random() * ((window.innerHeight - event.currentTarget.clientHeight) - 50)
+  //   })
+  // }
 
   return (
-      <div>
-        <button style={{position: "absolute"}} id={"btn"} onClick={(event) => buttonClicked(event)}>Click Me!</button>
+      <div className={"app-container"}>
+          <Route path={"/"} exact component={Home}/>
       </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from "@mui/material/Button"
+import {useState, useEffect} from "react";
+
+function Home() {
+    return (
+        <div className={"main-container d-flex flex-column justify-content-center align-items-center"}>
+            <h1 className={"landing-title"}>Skull King Online</h1>
+        </div>
+    )
+}
+
+export default Home
